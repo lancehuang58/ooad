c05e73fec71194c167251cbbe8932bbe0d33b26b
Lance Huang <lance.huang@deltaww.com>
2023-04-07 18:14:27 +0800

    abc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         showdown();
 
-//        unoGame();
+        unoGame();
     }
 
     private static void unoGame() {
--- src/main/java/player/AiPlayer.java
+++ src/main/java/player/AiPlayer.java
@@ -6,7 +6,7 @@ import net.bytebuddy.utility.RandomString;
 public class AiPlayer<T extends Card> extends Player<T> {
 
     public AiPlayer() {
-        this.hand = new Hand();
+        this.hand = new Hand<>();
         naming();
     }
 
