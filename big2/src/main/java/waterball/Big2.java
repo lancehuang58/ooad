b3fc3842fc9963c94c2e0b27e80bf18013da84fe
Lance Huang <lance.huang@deltaww.com>
2023-04-13 10:26:01 +0800

    #big2 reset

---
+++ big2/src/main/java/waterball/Big2.java
@@ -0,0 +1,5 @@
+package waterball;
+
+public class Big2 {
+
+}
---
+++ big2/src/test/java/waterball/Big2Test.java
@@ -0,0 +1,14 @@
+package waterball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Big2Test {
+    @Test
+    void test() {
+        
+        
+    }
+
+}
